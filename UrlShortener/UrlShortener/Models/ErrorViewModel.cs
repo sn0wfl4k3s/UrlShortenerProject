7bf88f3500ba716916d6a2ae4c9ede67e7d21cd4
Eduardo Júnior <ed.afram@gmail.com>
2018-12-25 12:31:41 -0200

    projeto criado

---
+++ UrlShortener/UrlShortener/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UrlShortener.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
