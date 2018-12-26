d84e4d5d141082d0e6fa73dd1ba5b7609d8caf5e
Eduardo Júnior <ed.afram@gmail.com>
2018-12-26 00:29:24 -0200

    adicionado a action Cadastrar

--- UrlShortener/UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/UrlShortener/Controllers/HomeController.cs
@@ -10,29 +10,29 @@ namespace UrlShortener.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Cadastrar (string url)
         {
-            return View();
+            ViewData["url"] = url;
+            return RedirectToAction("Result");
         }
 
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Result() => View();
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ UrlShortener/UrlShortener/Models/URL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Models
+{
+    public class URL
+    {
+        public int Id { get; set; }
+        public string UrlShort { get; set; }
+        public string UrlLong { get; set; }
+    }
+}
